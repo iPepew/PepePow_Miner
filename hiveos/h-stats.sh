62adfb66bba9dae78faf56e2017001f0829d8d40
iPepew <fima7385@mail.ru>
2026-07-26 11:29:00 +0300

    Fix HiveOS packaging for v0.1.1-rc2
    * Add HiveOS statistics callback
    
    * Update HiveOS manifest for rc2
    
    * Build correct HiveOS rc2 package

---
+++ hiveos/h-stats.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# PepePow Miner v0.1.1-rc2 does not expose a stable local statistics API yet.
+# HiveOS expects this callback to define both variables on every invocation.
+khs=0
+stats="null"
