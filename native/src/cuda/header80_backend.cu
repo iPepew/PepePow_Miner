101c22f4f951e62ccf361103d8a0f2f8b5e242f9
iPepew <fima7385@mail.ru>
2026-08-11 00:43:05 +0300

    feat: wire v1.0.6 async3 kernel into CUDA translation unit

--- native/src/cuda/header80_backend.cu
+++ native/src/cuda/header80_backend.cu
@@ -1,4 +1,4 @@
-// PepeW Miner 1.0.5 validated CUDA translation unit.
+// PepeW Miner 1.0.6 validated CUDA translation unit.
 // The implementation is split into ordered include fragments to keep the
 // consensus-tested source reproducible while allowing release-specific
 // runtime launch geometry and architecture support.
@@ -9,5 +9,6 @@
 #include "v1/header80_backend_part03.inc"
 #include "v1/header80_backend_part04.inc"
 #include "v1/header80_backend_part05.inc"
+#include "v1/header80_async3_v106.inc"
 #include "v1/header80_backend_part06.inc"
 #include "v1/header80_backend_part07.inc"
