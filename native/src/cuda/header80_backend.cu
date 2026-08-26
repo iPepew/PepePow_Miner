e1bc9a1bd5c6e9742b7eaf85718c1402bbfa1f09
iPepew <fima7385@mail.ru>
2026-08-26 14:25:44 +0300

    cuda: compile guarded service4warp production kernel beside baseline

--- native/src/cuda/header80_backend.cu
+++ native/src/cuda/header80_backend.cu
@@ -1,6 +1,7 @@
 // PepeW Miner 1.0.0 validated service768 CUDA translation unit.
 // The implementation is split into ordered include fragments to keep the
 // consensus-tested source byte-for-byte reproducible in the release branch.
+#include "guarded_lut_primitives.cuh"
 #include "v1/header80_backend_part00.inc"
 #include "v1/header80_backend_part01.inc"
 #include "v1/header80_backend_part02.inc"
@@ -8,4 +9,5 @@
 #include "v1/header80_backend_part04.inc"
 #include "v1/header80_backend_part05.inc"
 #include "v1/header80_backend_part06.inc"
+#include "service4warp_duallut_production.inc"
 #include "v1/header80_backend_part07.inc"
