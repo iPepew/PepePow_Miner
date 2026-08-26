d368c9d81e89cdf49ddfe62286fe2bcf51002088
iPepew <fima7385@mail.ru>
2026-08-26 03:23:32 +0300

    cuda: wire guarded LUT production kernel include

--- native/src/cuda/header80_backend.cu
+++ native/src/cuda/header80_backend.cu
@@ -1,11 +1,18 @@
 // PepeW Miner 1.0.0 validated service768 CUDA translation unit.
 // The implementation is split into ordered include fragments to keep the
 // consensus-tested source byte-for-byte reproducible in the release branch.
+#include "guarded_lut_primitives.cuh"
+
+#ifndef PEPEPOW_CUDA_GUARDED_LUT
+#define PEPEPOW_CUDA_GUARDED_LUT 0
+#endif
+
 #include "v1/header80_backend_part00.inc"
 #include "v1/header80_backend_part01.inc"
 #include "v1/header80_backend_part02.inc"
 #include "v1/header80_backend_part03.inc"
 #include "v1/header80_backend_part04.inc"
+#include "v1/header80_guarded_lut_path.inc"
 #include "v1/header80_backend_part05.inc"
 #include "v1/header80_backend_part06.inc"
 #include "v1/header80_backend_part07.inc"
