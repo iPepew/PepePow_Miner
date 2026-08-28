60846dfb230bc0164ef26a916e9a07a78d86dc81
iPepew <fima7385@mail.ru>
2026-08-28 05:17:43 +0300

    profile: allow host stage timing output

--- native/src/cuda/header80_backend.cu
+++ native/src/cuda/header80_backend.cu
@@ -1,6 +1,7 @@
 // PepeW Miner 1.0.0 validated service768 CUDA translation unit.
 // The implementation is split into ordered include fragments to keep the
 // consensus-tested source byte-for-byte reproducible in the release branch.
+#include <cstdio>
 #include "v1/header80_backend_part00.inc"
 #include "v1/header80_backend_part01.inc"
 #include "v1/header80_backend_part02.inc"
